0998901af71e5e75f5cbf37e2e6ab129954e67f8
Benjamin Phillips <benjamingt537@gmail.com>
2023-07-11 11:29:26 -0400

    added zooming and trying to get a move feature working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,30 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import World from "@/assets/world_4.svg"
 
 
 export default function Home() {
+	const svgRef = useRef()
 
 	const [title, setTitle] = useState("null")
 	const [pos, setPos] = useState({ x: 0, y: 0 })
 	const [state, setState] = useState(false)
+	const [scale, setScale] = useState(1)
+	const [origin, setOrigin] = useState({ x: 0, y: 0 })
+	const [moveState, setMoveState] = useState(false)
+	const [moveInit, setMoveInit] = useState({ x: 0, y: 0 })
+	const [moveFinal, setMoveFinal] = useState({ x: 0, y: 0 })
+	const [movePos, setMovePos] = useState({ x: 0, y: 0 })
 
 	useEffect(() => {
 		const paths = document.querySelectorAll("path")
 		console.log(paths)
 
 		paths.forEach(element => {
-			element.addEventListener("mousemove", (event) => {
+			element.addEventListener("mouseenter", () => {
 				setTitle(element.getAttribute("title"))
-				setPos({ x: event.clientX, y: event.clientY })
 				setState(true)
 			})
 			element.addEventListener("mouseout", () => {
@@ -26,17 +32,69 @@ export default function Home() {
 			})
 		})
 
+		window.addEventListener("mousemove", (event) => {
+			setPos({ x: event.clientX, y: event.clientY })
+		})
+
+		window.addEventListener("wheel", (event) => {
+			if (event.deltaY > 0) {
+				setScale(prev => Math.max(1, prev - 0.05))
+			} else {
+				setScale(prev => Math.min(3, prev + 0.05))
+			}
+			setOrigin({ x: event.clientX, y: event.clientY })
+		})
+
+		window.addEventListener("mousedown", (event) => {
+			setMoveState(true)
+			setMoveInit({ x: event.clientX, y: event.clientY })
+		})
+
+		window.addEventListener("mouseup", (event) => {
+			setMoveState(false)
+			setMoveFinal({ x: event.clientX, y: event.clientY })
+		})
+
 	}, [])
 
+	useEffect(() => {
+		if (moveState === true) {
+			window.addEventListener("mousemove", e => {
+				setMovePos({ x: e.clientX, y: e.clientY })
+			})
+		}
+
+		return () => window.removeEventListener("mousemove", e => {
+			setMovePos({ x: e.clientX, y: e.clientY })
+		})
+	}, [moveState])
+
 	return (
-		<main className="grid place-items-center h-screen bg-slate-300">
+		<main className="grid place-items-center h-screen bg-slate-300 overflow-hidden select-none">
+			<div className='absolute top-0 left-0 flex flex-col gap-1 z-10'>
+				<p className='bg-red-400 text-yellow-400'>{JSON.stringify(moveState, null, 4)}</p>
+				<p className='bg-red-400 text-yellow-400'>{JSON.stringify(moveInit, null, 4)}</p>
+				<p className='bg-red-400 text-yellow-400'>{JSON.stringify(moveFinal, null, 4)}</p>
+				<p className='bg-red-400 text-yellow-400'>{JSON.stringify(movePos, null, 4)}</p>
+				<p className='bg-red-400 text-yellow-400'>{JSON.stringify(pos, null, 4)}</p>
+			</div>
 			<p
-				className='absolute top-0 left-0 px-2 py-1 bg-white rounded-md shadow-md pointer-events-none transition-all ease-out'
-				style={{ transform: `translate(calc(${pos.x}px - 50%), calc(${pos.y}px - 125%))`, opacity: `${state ? 1 : 0}` }}
+				className='select-none absolute z-10 text-slate-700 top-0 left-0 px-2 py-1 bg-white rounded-md shadow-md pointer-events-none transition-all ease-out'
+				style={{ 
+					transform: `translate(calc(${pos.x}px - 50%), calc(${pos.y}px - 125%))`, 
+					opacity: `${state ? 1 : 0}`,
+					transformOrigin: `${pos.x}px ${pos.y}px`
+				}}
 			>
 				{title}
 			</p>
-			<World className="world_map" />
+			<World
+				className="world_map ease-out transition-all"
+				style={{
+					transform: `scale(${scale}) translate(${moveInit.x - movePos.x}px, ${moveInit.y - movePos.y}px)`,
+					// transformOrigin: `${origin.x}px ${origin.y/2}px`
+				}}	
+			/>
 		</main>
 	)
 }
